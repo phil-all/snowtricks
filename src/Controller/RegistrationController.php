e0bf10ce58f78d832df266acbaa894fe9e07716c
phil-all <allard.latour.philippe@gmail.com>
2022-01-19 21:30:15 +0100

    :arrow_up: Update registration process #12

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\TemplateMailer;
+use App\Service\JwtTokenHandler;
 use App\Form\RegistrationFormType;
-use App\Repository\StatusRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,38 +20,88 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_register")
+     * @var JwtTokenHandler
+     */
+    private JwtTokenHandler $token;
+
+    /**
+     * RegistrationController constructor
+     *
+     * @param JwtTokenHandler $token
+     */
+    public function __construct(JwtTokenHandler $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @Route("/inscription", name="app_register")
      *
      * @param Request $request
-     * @param UserPasswordHasherInterface $userPasswordHasher
-     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $userRepository
+     * @param TemplateMailer $mailer
+     * @param UserPasswordHasherInterface $passwordHasher
      *
      * @return Response
      */
     public function register(
         Request $request,
-        UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager,
-        StatusRepository $status
+        UserRepository $userRepository,
+        TemplateMailer $mailer,
+        UserPasswordHasherInterface $passwordHasher
     ): Response {
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setStatus($status->findOneByStatus('pending'));
-            $user->setRoles(['ROLE_USER']);
-            $user->setPassword($userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData()));
+            $userRepository->createPendingUser($user, $passwordHasher, $form->get('plainPassword')->getData());
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+            $mailer->sendValidationMail($user, $this->token);
 
-            return $this->redirectToRoute('home');
+            return $this->render('messages/waiting-validation-account.html.twig', [
+                'email' => $user->getEmail(),
+            ]);
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/validation/{header}/{payload}/{signature}", name="app_account_validation")
+     *
+     * @param string $header
+     * @param string $payload
+     * @param string $signature
+     *
+     * @return Response
+     */
+    public function validation(
+        string $header,
+        string $payload,
+        string $signature,
+        UserRepository $userRepository
+    ): Response {
+
+        $token = $header . '.' . $payload . '.' . $signature;
+
+        if (!$this->token->tokenChecker($token)) {
+            return $this->render('messages/invalid-link.html.twig', []);
+        }
+
+        $email  = $this->token->getMail($token);
+        $user   = $userRepository->findOneByEmail($email);
+        $status = $user->getStatus();
+
+        if ($status->getId() !== 1) {
+            return $this->render('messages/user-already-validated.html.twig', []);
+        }
+
+        $userRepository->userActivation($user);
+
+        return $this->render('messages/account-confirmation.html.twig', []);
     }
 }
