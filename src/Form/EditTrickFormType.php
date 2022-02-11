de6605da2518481ae98a554cda904b6df198cc50
phil-all <allard.latour.philippe@gmail.com>
2022-02-11 01:53:56 +0100

    :arrow_up: Update trick form and create collection forms

---
+++ src/Form/EditTrickFormType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class EditTrickFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $cat = $options['reorderedCategory'];
+
+        $builder
+            ->add('title', TextType::class, ['label' => 'Nom de la figure :'])
+            ->add('content', TextareaType::class, [
+                'label' => 'Description :',
+                'attr'  => ['rows' => '10'],
+            ])
+            ->add('category', EntityType::class, [
+                'class'        => Category::class,
+                'label'        => 'Groupe :',
+                'choice_label' => 'category',
+                'choice_value' => 'id',
+                'choices'      => $cat,
+            ])
+            ->add('thumbnail', FileType::class, [
+                'mapped'   => false,
+                'required' => false,
+                'label'    => false,
+            ])
+            ->add('images', CollectionType::class, [
+                'entry_type'    => ImageFormType::class,
+                'entry_options' => ['label' => false],
+                'allow_add'     => true,
+                'required'      => false,
+                'label'         => false,
+            ])
+            ->add('videos', CollectionType::class, [
+                'entry_type'    => VideoFormType::class,
+                'entry_options' => ['label' => false],
+                'allow_add'     => true,
+                'required'      => false,
+                'label'         => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class'        => Trick::class,
+            'reorderedCategory' => false,
+        ]);
+    }
+}
